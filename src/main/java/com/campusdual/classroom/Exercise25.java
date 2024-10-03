6ef39bd7379fa719344ea23d3cc7d49d157fc331
Beatriz Faro Carrera <farocarrera.beatriz@gmail.com>
2024-10-03 19:33:19 +0200

    Exercicio 25 resolto

--- src/main/java/com/campusdual/classroom/Exercise25.java
+++ src/main/java/com/campusdual/classroom/Exercise25.java
@@ -4,15 +4,26 @@ import java.util.Stack;
 
 public class Exercise25 {
     public static Stack<String> createStack() {
+        Stack<String> pila = new Stack<>();
 
+        pila.push("Smith");
+        pila.push("Montessori");
+        pila.push("Peralta");
+        pila.push("House");
+
+        return pila;
     }
 
     public static void printAndEmptyStack(Stack<String> stack) {
-
+        while (!stack.isEmpty()) {
+            System.out.println(stack.pop());
+        }
     }
 
     public static void main(String[] args) {
-
+        Stack<String> pila = createStack();
+        System.out.println("El elemento que se encuentra en la cima de la pila es: " + pila.peek());
+        printAndEmptyStack(pila);
     }
 
 
